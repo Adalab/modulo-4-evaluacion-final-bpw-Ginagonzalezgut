57d558df2a9339c6dcfa5650db57f0b28e0830b6
Gina González <gini_go@hotmail.com>
2024-09-18 11:56:22 +0200

    add get and post endpoints

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ require("dotenv").config();
 const server = express();
 
 server.use(cors());
+server.use(express.json());
+
 server.set("view engine", "ejs");
 
 const port = 5001;
@@ -18,9 +20,83 @@ async function getDBConnection() {
   const connection = await mysql.createConnection({
     host: "localhost",
     database: "shoestore",
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    user: "root",
+    password: "Ginackck",
   });
   connection.connect();
   return connection;
 }
+
+server.get("/marcas", async (req, res) => {
+  try {
+    const connection = await getDBConnection();
+    const queryMarcas = "SELECT * FROM marcas";
+    const [result] = await connection.query(queryMarcas);
+
+    connection.end();
+
+    res.status(200).json({
+      info: "sucess",
+      results: result,
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: "error",
+      message: "Internal server error",
+    });
+  }
+});
+
+server.get("/productos", async (req, res) => {
+  try {
+    const connection = await getDBConnection();
+    const QueryProductos = "SELECT * FROM productos";
+    const [result] = await connection.query(QueryProductos);
+
+    connection.end();
+
+    res.status(200).json({
+      info: "sucess",
+      results: result,
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: "error",
+      message: "Internal server error",
+    });
+  }
+});
+
+server.post("/marcas", async (req, res) => {
+  const { nombre, logo_url } = req.body;
+  const connection = await getDBConnection();
+  const QueryMarcas = "INSERT INTO marcas(nombre,logo_url)VALUES(?,?)";
+  const [result] = await connection.query(QueryMarcas, [nombre, logo_url]);
+  connection.end();
+  res.status(201).json({
+    status: "sucess",
+    message: result,
+  });
+});
+
+server.get("/marcas/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const connection = await getDBConnection();
+    const queryMarcas = "SELECT * FROM marcas WHERE id_marca=?";
+    const [result] = await connection.query(queryMarcas, [id]);
+
+    connection.end();
+
+    res.status(200).json({
+      info: "sucess",
+      results: result,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      status: "error",
+      message: "Internal server error",
+    });
+  }
+});
